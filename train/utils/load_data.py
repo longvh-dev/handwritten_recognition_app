988debe49435c41d201efe21c530f1bb5e2cbb17
longvh-dev <longvh.work@gmail.com>
2024-02-27 00:33:25 +0700

    fix: fix datasets bug

--- train/utils/load_data.py
+++ train/utils/load_data.py
@@ -17,10 +17,10 @@ def load_data(batch_size = 64):
         transforms.Normalize((0.5,), (0.5,))
     ])
 
-    train_dataset = datasets.datasets.MNIST(root = './data', train = True,
+    train_dataset = datasets.MNIST(root = './data', train = True,
                                             download = True,
                                             transform = transform)
-    test_dataset = datasets.datasets.MNIST(root = './data', train = False,
+    test_dataset = datasets.MNIST(root = './data', train = False,
                                            download = True,
                                            transform = transform)
 
