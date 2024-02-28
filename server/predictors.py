6386d297d9e9e3adf5cd9b18565eeac9bd90e58b
longvh-dev <longvh.work@gmail.com>
2024-02-28 21:29:20 +0700

    fix: remove unnecessary file

--- server/predictors.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-
-def predict_digit(input, model_path):
-    model = torch.load(model_path)
-    model.eval()
-    with torch.no_grad():
-        output = model(torch.tensor(input, dtype = torch.float32))
-    return output.argmax().item()
